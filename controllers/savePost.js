adccc25152780cc46764c4a1f1abd810e7bbd5bb
Suvang Samal <suvang08@gmail.com>
2022-09-18 01:29:08 +0530

    removed saved property

--- controllers/savePost.js
+++ controllers/savePost.js
@@ -1,8 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const User = require("../models/User");
-const channelModel = require("../models/Channel");
-const Category = require("../models/Allcategories");
 const getUserWithPosts = require("../middleware/getUserWithPosts");
 
 //desc    adds a highlighted story id to the user
@@ -23,18 +21,9 @@ exports.savePost = asyncHandler(async (req, res, next) => {
     );
 
     if (isExist) {
-      if (post.categoryType === "image") {
-        const categoryItem = await Category.findById(post.id);
-        categoryItem.saved = false;
-        await categoryItem.save();
-      } else if (post.categoryType === "video") {
-        const videoItem = await channelModel.findById(post.id);
-        videoItem.saved = false;
-        await videoItem.save();
-      }
-
       const index = user.savedPosts.findIndex(
-        (item) => item.id === post.id && item.categoryType === post.categoryType
+        (item) =>
+          item._id === post.id && item.categoryType === post.categoryType
       );
 
       user.savedPosts.splice(index, 1);
@@ -46,16 +35,6 @@ exports.savePost = asyncHandler(async (req, res, next) => {
     } else {
       user.savedPosts.push(post);
       await user.save();
-
-      if (post.categoryType === "image") {
-        const categoryItem = await Category.findById(post.id);
-        categoryItem.saved = true;
-        await categoryItem.save();
-      } else if (post.categoryType === "video") {
-        const videoItem = await channelModel.findById(post.id);
-        videoItem.saved = true;
-        await videoItem.save();
-      }
 
       const result = await getUserWithPosts(user);
 
