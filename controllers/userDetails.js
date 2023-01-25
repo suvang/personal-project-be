151d7b3e35d6af06f3d76ebd72772937d680f617
Suvang Samal <suvang08@gmail.com>
2023-01-25 18:55:55 +0530

    httponly update

--- controllers/userDetails.js
+++ controllers/userDetails.js
@@ -253,7 +253,7 @@ exports.loginUser = asyncHandler(async (req, res, next) => {
 
     const options = {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
+      httpOnly: false,
     };
 
     res.status(200).cookie("token", token, options).json({
