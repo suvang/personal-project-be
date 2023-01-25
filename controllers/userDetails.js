f43c7db90cb9f88beb6b8795c80c708ad82ced18
Suvang Samal <suvang08@gmail.com>
2023-01-25 19:16:24 +0530

    secure & samesite options

--- controllers/userDetails.js
+++ controllers/userDetails.js
@@ -254,6 +254,8 @@ exports.loginUser = asyncHandler(async (req, res, next) => {
     const options = {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       httpOnly: false,
+      secure: true,
+      sameSite: "none",
     };
 
     res.status(200).cookie("token", token, options).json({
